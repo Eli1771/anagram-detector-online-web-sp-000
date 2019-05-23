f38286f6a3202b651a1a59967a289539b8cb4058
elijahmm17 <elijahmm17@gmail.com>
2019-05-23 20:17:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,14 @@
 # Your code goes here!
 class Anagram 
+  attr_read :word 
   
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(phrase) 
+    phrase.each do |w| 
+      if @word.sort
+    end 
+  end 
 end 
